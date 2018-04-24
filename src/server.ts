9771044c7a5606b4ff16cfb5b942d8328e6bcf70
Wes Peter <wes@wespeter.com>
2018-04-23 17:03:49 -0700

    add some prettier

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import graphqlHTTP from 'express-graphql';
-import { buildSchema } from 'graphql';
+import express from "express";
+import expressGraphql from "express-graphql";
+import { buildSchema } from "graphql";
 
 const PORT = process.env.PORT || 5000;
+const wes = "hi";
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -14,20 +15,22 @@ const schema = buildSchema(`
 // The root provides a resolver function for each API endpoint
 const root = {
   hello: () => {
-    console.log('hello resolver called');
-    return 'Hello world!';
-  },
+    // tslint:disable-next-line:no-console
+    console.log("hello resolver called");
+    return "Hello world!";
+  }
 };
 
 const app = express();
 app.use(
-  '/graphql',
-  graphqlHTTP({
+  "/graphql",
+  expressGraphql({
     schema,
     rootValue: root,
-    graphiql: true,
-  }),
+    graphiql: true
+  })
 );
 
 app.listen(PORT);
+// tslint:disable-next-line:no-console
 console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);

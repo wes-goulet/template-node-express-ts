64b33d8b3f625d19b21416449641442f620add84
Wes Peter <wes@wespeter.com>
2019-06-25 17:22:38 -0700

    remove graphql stuff

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # template-ts-node
-Starter repo for node+express+graphql+typescript+tslint+vscode debug configs
+Starter repo for node+express+typescript+vscode debug configs
 
 ## How to build
 
 ```
-yarn build
+npm run build
 ```
 
 ## How to debug
 
-To launch nodemon debug session from vscode use the `nodemon` debug configuration. To manually start a debug nodemon session that can be attached to run `yarn debug`.
+To launch nodemon debug session from vscode use the `nodemon` debug configuration. To manually start a debug nodemon session that can be attached to run `npm run debug`.
 
 ## References
 - [Microsoft/TypeSript-Node-Starter](https://github.com/Microsoft/TypeScript-Node-Starter)
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,8 @@
 import express from "express";
-import expressGraphql from "express-graphql";
-import { buildSchema } from "graphql";
 
 const PORT = process.env.PORT || 5000;
-
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String,
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    // tslint:disable-next-line:no-console
-    console.log("hello resolver called");
-    return "Hello world!";
-  }
-};
-
 const app = express();
-app.use(
-  "/graphql",
-  expressGraphql({
-    schema,
-    rootValue: root,
-    graphiql: true
-  })
-);
 
-app.listen(PORT);
-// tslint:disable-next-line:no-console
-console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);
+app.get("/", (_req, res) => res.send("Hello World"));
+
+app.listen(PORT, () => console.log(`Running server at localhost:${PORT}`));
